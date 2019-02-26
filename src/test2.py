974ef1375ff266236e5738f22c7c0bfb889c8922
8937 <yanjun@adpanshi.com>
2019-02-26 12:55:24 +0800

    111

--- src/test2.py
+++ src/test2.py
@@ -1,8 +1,12 @@
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, error
+# address = "F:\mygithub\change_mp3\data/first.mp3"
+# address = "F:\mygithub\change_mp3\data/pick_bila_waktu_telah_berakhir.mp3"
+# address = "F:\mygithub\change_mp3\data/pick_taubat.mp3"
+address = "F:\mygithub\change_mp3\data/sholawat_nabi_maulidu_ahmad.mp3"
 
-audio = MP3('F:\mygithub\change_mp3\data\pick_bila_waktu_telah_berakhir.mp3', ID3=ID3)
+audio = MP3(address, ID3=ID3)
 
 try:
     audio.add_tags()
@@ -20,7 +24,7 @@ audio.tags.add(
 )
 
 audio.save()
-audio = EasyID3('F:\mygithub\change_mp3\data\pick_bila_waktu_telah_berakhir.mp3')
+audio = EasyID3(address)
 audio['artist'] = "artist"
 audio['title'] = "title"
 audio['tracknumber'] = "tracknumber"
